f6d5a7f5ca7d86a5e42cc19d355dde62810d3355
Rizwan Walayat <rizwan.wallayat@gmail.com>
2025-07-29 00:53:12 +0500

    Initial Commit

---
+++ Flite Spotify SwiftUI/Flite_Spotify_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flite_Spotify_SwiftUIApp.swift
+//  Flite Spotify SwiftUI
+//
+//  Created by Techtiz PM on 29/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Flite_Spotify_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
